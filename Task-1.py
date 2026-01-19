d9b745555eb9623ef481f2bc05135601e3ec3483
Sachinace033 <sachu9444271676@gmail.com>
2026-01-19 20:52:13 +0530

    File updated

--- Task-1.py
+++ Task-1.py
@@ -4,18 +4,18 @@
 #Arithmetic Operations:
    #Simple calculations
 '''
-print ( "Add      :", a+b)
-print ( "Subtract :", a-b)
-print ( "Multiply :", a*b)
-print ( "Divide   :", a/b)
-print ( "Modulus  :", a%b)
+print ("Add      :", a+b)
+print ("Subtract :", a-b)
+print ("Multiply :", a*b)
+print ("Divide   :", a/b)
+print ("Modulus  :", a%b)
 '''
     #Total and average calculation
 '''
 total = a + b
 average = total / 2
-print ( "Total :", total)
-print ( "Average :", average)
+print ("Total :", total)
+print ("Average :", average)
 '''
 
 
@@ -25,31 +25,33 @@ print ( "Average :", average)
 '''
 a=int(input("Enter the Age: "))
 if a >= 18:
-    print ( "He is eligible for vote...!")
+    print ("He is eligible for vote...!")
 else:
-    print ( "He is not eligible for vote...!")
+    print ("He is not eligible for vote...!")
 '''
     #Number comparison
 '''
 a=int(input("Enter the 1st number:- "))
 b=int(input("Enter the 2nd number:- "))
 if a > b:
-    print ( "1st number is greater than 2nd number")
+    print ("1st number is greater than 2nd number")
 elif a <= b:
-    print ( "2nd number is greater than 1st number")
+    print ("2nd number is greater than 1st number")
 elif a == b:
-    print ( "Both numbers are equal")
+    print ("Both numbers are equal")
+else:
+    print ("Both numbers are not equal")
 '''
     #Demonstration of all comparison operators
 '''
 a= int(input("Enter first number: "))
 b= int(input("Enter second number: "))
-print ( "Is a equal to b? :", a==b)
-print ( "Is a not equal to b? :", a!=b)
-print ( "Is a greater than b? :", a>b)
-print ( "Is b less than a? :", b<a)
-print ( "Is a greater than or equal to b? :", a>=b)
-print ( "Is b less than or equal to a? :", b<=a)
+print ("Is a equal to b? :", a==b)
+print ("Is a not equal to b? :", a!=b)
+print ("Is a greater than b? :", a>b)
+print ("Is b less than a? :", b<a)
+print ("Is a greater than or equal to b? :", a>=b)
+print ("Is b less than or equal to a? :", b<=a)
 '''
 
 
@@ -85,4 +87,17 @@ print("After /= :", x)
 
 x %= 3
 print("After %= :", x)'''
+
+
+#Bitwise Operators
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Bitwise AND (&):", a & b)
+print("Bitwise OR (|):", a | b)
+print("Bitwise XOR (^):", a ^ b)
+print("Bitwise NOT (~) of a:", ~a)
+print("Bitwise NOT (~) of b:", ~b)
+print("Left Shift (a << 2):", a << 2)
+print("Right Shift (b >> 2):", b >> 2)
+
 
