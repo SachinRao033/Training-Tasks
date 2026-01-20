4daefeaf216fdce6af9148566e799d481e56271b
Sachinace033 <sachu9444271676@gmail.com>
2026-01-20 18:10:45 +0530

    Tasks file updated

--- Task-1.py
+++ Task-1.py
@@ -30,8 +30,8 @@ else:
     print ("He is not eligible for vote...!")
 '''
     #Number comparison
-'''
-a=int(input("Enter the 1st number:- "))
+
+'''a=int(input("Enter the 1st number:- "))
 b=int(input("Enter the 2nd number:- "))
 if a > b:
     print ("1st number is greater than 2nd number")
@@ -40,8 +40,8 @@ elif a <= b:
 elif a == b:
     print ("Both numbers are equal")
 else:
-    print ("Both numbers are not equal")
-'''
+    print ("Both numbers are not equal")'''
+
     #Demonstration of all comparison operators
 '''
 a= int(input("Enter first number: "))
@@ -70,7 +70,8 @@ else:
 
 
 #Assignment Operators
-'''x = int(input("Enter initial value of x: "))
+'''
+x = int(input("Enter initial value of x: "))
 print("Initial value:", x)
 
 x += 5
@@ -86,10 +87,12 @@ x /= 4
 print("After /= :", x)
 
 x %= 3
-print("After %= :", x)'''
+print("After %= :", x)
+'''
 
 
 #Bitwise Operators
+'''
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 print("Bitwise AND (&):", a & b)
@@ -99,5 +102,41 @@ print("Bitwise NOT (~) of a:", ~a)
 print("Bitwise NOT (~) of b:", ~b)
 print("Left Shift (a << 2):", a << 2)
 print("Right Shift (b >> 2):", b >> 2)
+'''
+
+#Range of numbers 0-100 print numbers divisible by 3 or 5, if divisible by both print 'py'
+
+for i in range(1,101):
+  if i%3==0 and i%5==0:
+    print('py',end=' ')
+  elif i%3==0 or i%5==0:
+    print(i , end=' ')
+    continue
+else:
+    print(i)
 
 
+
+#Print first 10 natural numbers using while loop
+'''i=1
+while i<=10:
+    print(i , end=' ')
+    i+=1
+print()
+'''
+
+#Print first 10 natural numbers using for loop
+'''for i in range(1,11):
+    print(i , end=' ')'''
+
+#Print pattern
+'''
+rows=int(input("Enter number of rows: "))
+for i in range(rows):
+  for j in range(rows):
+        if( j==0 or j==rows-1 or (i==j and i<=rows//2)or (i+j==rows-1 and i<=2)):
+            print('*', end=' ')
+        else:
+            print(end=' ')
+print()
+'''
